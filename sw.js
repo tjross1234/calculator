8a50f945288e58d65847c83950211f474a0ff02a
Trevor Ross <Trevor.Ross@vale.com>
2018-09-16 13:51:49 -0400

    fixes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
 importScripts('sw-toolbox.js');
 toolbox.precache(["calc.html","calc.css", "calc.js"]);
-toolbox.router.get('/calculator/images/*', toolbox.cacheFirst);
-toolbox.router.get('/calculator/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
